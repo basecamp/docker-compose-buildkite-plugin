effb59a763f11ff8d36b0f14fd082714ef8833dd
Nathan Anderson <andnat@gmail.com>
2017-02-02 12:33:51 -0500

    Add support for multiple tags

--- hooks/commands/push.sh
+++ hooks/commands/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMPOSE_SERVICE_NAME="$BUILDKITE_PLUGIN_DOCKER_COMPOSE_RUN"
+COMPOSE_SERVICE_NAME="$BUILDKITE_PLUGIN_DOCKER_COMPOSE_PUSH"
 local current_image="$(buildkite-agent meta-data get "$(build_meta_data_image_tag_key "$COMPOSE_SERVICE_NAME")" 2>/dev/null)"
 local SHORT_SHA=$(git rev-parse --short ${BUILDKITE_COMMIT})
 local SANITIZED_BRANCH=$(echo $BUILDKITE_BRANCH|tr '/' '-')
@@ -15,10 +15,28 @@ try_image_restore_from_docker_repository() {
   fi
 }
 
+filter_tag_name() {
+  case $1 in
+    short_sha)
+      echo $SHORT_SHA
+      ;;
+    branch_name)
+      echo $SANITIZED_BRANCH
+      ;;
+    *)
+      echo $1
+      ;;
+  esac
+}
+
 try_image_restore_from_docker_repository "$current_image"
 
-echo "+++ :docker: Test Push"
+echo "+++ :docker: Push Custom Tags"
+echo "+++ debug: $current_image"
 
-plugin_prompt_and_must_run docker tag "$current_image" $BUILDKITE_PLUGIN_DOCKER_COMPOSE_IMAGE_REPOSITORY:$SHORT_SHA
-
-exit 0
+for tagvar in $(env|grep 'BUILDKITE_PLUGIN_DOCKER_COMPOSE_TAGS')
+do
+    local tag=$(filter_tag_name $(echo $tagvar | awk -F= '{print $2}'))
+    echo "+++ :docker: Pushing $tag"
+    plugin_prompt_and_must_run docker tag "$current_image" $BUILDKITE_PLUGIN_DOCKER_COMPOSE_IMAGE_REPOSITORY:$tag
+done
