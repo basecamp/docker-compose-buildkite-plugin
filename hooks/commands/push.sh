226a2f22f4347c97ed0bf281c5df1fdd2ba29d76
Nathan Anderson <andnat@gmail.com>
2017-02-02 12:38:15 -0500

    Fix improper use of local

--- hooks/commands/push.sh
+++ hooks/commands/push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 COMPOSE_SERVICE_NAME="$BUILDKITE_PLUGIN_DOCKER_COMPOSE_PUSH"
-local current_image="$(buildkite-agent meta-data get "$(build_meta_data_image_tag_key "$COMPOSE_SERVICE_NAME")" 2>/dev/null)"
-local SHORT_SHA=$(git rev-parse --short ${BUILDKITE_COMMIT})
-local SANITIZED_BRANCH=$(echo $BUILDKITE_BRANCH|tr '/' '-')
+CURRENT_IMAGE="$(buildkite-agent meta-data get "$(build_meta_data_image_tag_key "$COMPOSE_SERVICE_NAME")" 2>/dev/null)"
+SHORT_SHA=$(git rev-parse --short ${BUILDKITE_COMMIT})
+SANITIZED_BRANCH=$(echo $BUILDKITE_BRANCH|tr '/' '-')
 
 try_image_restore_from_docker_repository() {
   local image=$1
@@ -32,11 +32,11 @@ filter_tag_name() {
 try_image_restore_from_docker_repository "$current_image"
 
 echo "+++ :docker: Push Custom Tags"
-echo "+++ debug: $current_image"
+echo "+++ debug: $CURRENT_IMAGE"
 
 for tagvar in $(env|grep 'BUILDKITE_PLUGIN_DOCKER_COMPOSE_TAGS')
 do
     local tag=$(filter_tag_name $(echo $tagvar | awk -F= '{print $2}'))
     echo "+++ :docker: Pushing $tag"
-    plugin_prompt_and_must_run docker tag "$current_image" $BUILDKITE_PLUGIN_DOCKER_COMPOSE_IMAGE_REPOSITORY:$tag
+    plugin_prompt_and_must_run docker tag "$CURRENT_IMAGE" $BUILDKITE_PLUGIN_DOCKER_COMPOSE_IMAGE_REPOSITORY:$tag
 done
